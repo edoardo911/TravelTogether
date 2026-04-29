3a9f4e6cce82681730b10dfbec9f54359b9ca9fc
edoardo911 <edoardo.stucchi902@gmail.com>
2026-04-29 15:49:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Travel Together
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/unibg/travel_together/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.unibg.travel_together
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
